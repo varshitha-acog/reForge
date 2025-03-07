b5523d35fe44afd0edd801a7989fac5386445d17
DY <dan1054yang@gmail.com>
2025-03-07 17:40:49 +0500

    Update documentation

--- _downloads/64f1b5cb8521d625198dd2770ca2ea14/go_protein.py
+++ _downloads/64f1b5cb8521d625198dd2770ca2ea14/go_protein.py
@@ -11,66 +11,81 @@ Requirements:
 
 Author: DY
 """
+
 import os
 from reforge import cli
 from reforge.mdsystem.gmxmd import GmxSystem, GmxRun
 
-WDIR = '.' # '.' for html, 'examples' for manual
-os.chdir(WDIR)
-cli.run('rm -rf test/*')
 #%%
-# First, we need to initialize an instance of GmxSystem which will take care of pathing
-# and necessary files. This will use parent directory 'tests' relative to the current directory
-# and root directory 'test' in 'tests' for the system.
-mdsys = GmxSystem(sysdir='.', sysname='test')
+# First, we need to initialize an instance of *GmxSystem*, which will handle path management
+# and the necessary files. This instance uses the parent directory *'systems'* (relative to the current
+# directory) and the root directory *'test'* within *'systems'* for the system.
+mdsys = GmxSystem(sysdir='systems', sysname='test')
+
 #%%
-# This command will actually prepare the necessary files
+# Next, prepare the necessary files by calling *prepare_files()*.
 mdsys.prepare_files()
+
+# List the files in the system's root directory:
 for f in mdsys.root.iterdir():
     print(f)
+
 #%%
-# This will sort chains and atoms in our PDB to avoid conflicts in the future and make file 'inpdb.pdb',
-# which we can access later as mdsys.inpdb
-in_pdb = "../1btl.pdb" # relative to mdsys.root or absolute
+# Sort chains and atoms in the input PDB file to avoid future conflicts.
+# This creates a file (named *inpdb.pdb*) that can later be accessed as *mdsys.inpdb*.
+in_pdb = mdsys.root / "1btl.pdb"  # can be relative to *mdsys.root* or an absolute path
 mdsys.sort_input_pdb(in_pdb)
 print(mdsys.inpdb)
+
 #%%
-# Even though we don't need to have multiple chains in this case, this command splits our mdsys.inpdb
-# into separate chains and moves Proteins and RNA/DNA to their respective directories
+# Although there are multiple chains in this case, the *split_chains()* method
+# splits *mdsys.inpdb* into separate chains and moves protein and RNA/DNA files to their respective directories.
 mdsys.split_chains()
+
 #%%
-# Coarse-grain the proteins using martinize2 by Martini
-mdsys.martinize_proteins_go(go_eps=10.0, go_low=0.3, go_up=1.0, p="backbone", pf=500, append=False);
+# Coarse-grain the proteins using *martinize2* (by Martini) with the following parameters:
+#     *go_eps* = 10.0, *go_low* = 0.3, *go_up* = 1.0,
+#     *p* = "backbone", *pf* = 500, *append* = False
+mdsys.martinize_proteins_go(go_eps=10.0, go_low=0.3, go_up=1.0, p="backbone", pf=500, append=False)
+
 #%%
-# Let's take a look at the generated files. The topology should contain the main protein topology file 'chain_A.itp'
-# and virtual sites parameters for go-model - 'go_atomtypes.itp' and 'go_nbparams.itp'.
+# Inspect the generated topology files. The topology should include the main protein topology file
+# *chain_A.itp* as well as the virtual sites parameters for the GO model:
+# *go_atomtypes.itp* and *go_nbparams.itp*.
 for f in mdsys.topdir.iterdir():
     print(f)
-#%% 
-# And the coarse-grained structure should be in 'cgdir'.
-for f in mdsys.cgdir.iterdir():
-    print(f)
-#%% 
-# These two commands will combine all topology and structure files. "make_cg_structure" uses GROMACS "gmx pdb2gmx" 
-# module to make the simulation box, and its description can be found online 
-mdsys.make_cg_topology() # CG topology. Returns mdsys.systop ("mdsys.top") file
-mdsys.make_cg_structure(bt='dodecahedron', d='1.2', ) # CG structure. Returns mdsys.solupdb ("solute.pdb") file    
 
 #%% 
-# Now we need to add solvent and neutralize the system's charge
+# Check the coarse-grained structure in the *cgdir* directory.
+for f in mdsys.cgdir.iterdir():
+    print(f)
+
+#%% 
+# Combine all topology and structure files.
+# The method *make_cg_topology()* uses GROMACS's *gmx pdb2gmx* module to create the simulation box.
+# (See online documentation for details.)
+# It returns the CG topology as *mdsys.systop* (i.e. "mdsys.top").
+mdsys.make_cg_topology()
+
+# The method *make_cg_structure()* creates the CG structure.
+# Here, *bt* = 'dodecahedron' and *d* = '1.2'; it returns *mdsys.solupdb* (i.e. "solute.pdb").
+mdsys.make_cg_structure(bt='dodecahedron', d='1.2')
+
+#%% 
+# Add solvent and neutralize the system's charge.
 solvent = mdsys.root / "water.gro"
 mdsys.solvate(cp=mdsys.solupdb, cs=solvent)
 mdsys.add_bulk_ions(conc=0.15, pname="NA", nname="CL")
 
 #%% 
-# In order to work with GROMACS' selections we need to make a .ndx file mdsys.sysndx.
-# Order of the groups: 1.System,  2.Solute,  3.Backbone,  4.Solvent,  5. Not Water,  6-. Chains.
-# Custom groups can be added using AtomList.write_to_ndx() method
+# To work with GROMACS selections, generate an index file (*mdsys.sysndx*).
+# The default group order is: 1. *System*, 2. *Solute*, 3. *Backbone*, 4. *Solvent*, 5. *Not Water*, and then 6+ individual chains.
+# Custom groups can be added using the method *AtomList.write_to_ndx()*.
 mdsys.make_system_ndx(backbone_atoms=["BB"])
 
 #%%
-# One of the little quality of life features of GmxSystem is executing GROMACS commands from your python script
-# with GmxSystem.gmx. This will run the command in the working directory of the system.
-# To take a look at the system, we can use VMD or PyMol, but we need to correct the generated box for
-# the periodic boundary conditions, and we can do it with GROMACS' "trjconv" module. 
+# One of the convenient features of *GmxSystem* is the ability to execute GROMACS commands
+# directly from your Python script using *GmxSystem.gmx*. This runs the command in the system's working directory.
+# For example, to view the system (using VMD or PyMOL), you must first correct the generated box for
+# periodic boundary conditions. This is done with GROMACS's *trjconv* module:
 mdsys.gmx("trjconv", clinput='0\n', s=mdsys.syspdb, f=mdsys.syspdb, pbc='atom', ur='compact', o="viz.pdb")
